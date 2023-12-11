07ba44a82e28b519a5a7adb6f46e4b20256c43e4
nhat nhat <nhatnhat@nhats-MacBook-Air.local>
2023-12-11 10:05:37 +0700

    hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,23 +1,68 @@
 import React from 'react'
-
+import "./hero.sass"
+import { motion } from "framer-motion"
 const Hero = () => {
+    const textVariants = {
+        initial: {
+            x: -500,
+            opacity: 0
+
+        },
+        animate: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.1,
+                duration: 1
+            }
+
+        }, scrollButton: {
+            opacity: 0,
+            y: 10,
+            transition: {
+                duration: 2,
+                repeat: Infinity
+            }
+        }
+
+    }
+    const slideVariants = {
+        initial: {
+            x: 0,
+        },
+        animate: {
+            x: "-320%",
+            opacity: 1,
+            transition: {
+                repeat: Infinity,
+                duration: 20,
+                repeatType: "mirror"
+            }
+
+        }
+
+    }
     return (
         <div className='hero'>
             <div className="wrapper">
-                <div className="textContainer">
-                    <h2> NHAT NGUYEN</h2>
-                    <h1>Web Developer and UI Design </h1>
-                    <div className="button">
-                        <button type="button">See the latest work</button>
-                        <button type="button">Contact me</button>
-                    </div>
-                    <img src="/scroll.png" alt="" />
-                </div>
+                <motion.div className="textContainer" variants={textVariants} initial={"initial"} animate="animate">
+                    <motion.h2 variants={textVariants} > NHAT NGUYEN</motion.h2>
+                    <motion.h1 variants={textVariants}>Web Developer and UI Design </motion.h1>
+                    <motion.div className="buttons" variants={textVariants}>
+                        <motion.button variants={textVariants} type="button">See the latest work</motion.button>
+                        <motion.button variants={textVariants} type="button">Contact me</motion.button>
+                    </motion.div>
+                    <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" />
+                </motion.div>
             </div>
+            <motion.div className="slidingTextContainer" variants={slideVariants} initial="initial" animate="animate">
+                White Content Creator Influencer
+            </motion.div>
+
             <div className="imageContainer">
                 <img src="./hero.png" alt="" />
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/sidebar/Links/Links.jsx
+++ src/components/sidebar/Links/Links.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+import "./links.sass"
 
 const variants = {
   open: {
@@ -18,21 +19,23 @@ const variants = {
 
 const itemsVariants = {
   open: {
-    x: 0,
+    x: 1,
     opacity: 1,
+
   },
 
   closed: {
     x: -200,
     opacity: 0,
   },
+
 };
 
 const Links = () => {
   return (
     <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <motion.a href={`#${item}`} key={item} variants={itemsVariants} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+        <motion.a href={`#${item}`} key={item} variants={itemsVariants} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
           {item}
         </motion.a>
       ))}
